695564da74fe7c95802f5bf59e442e23a2d7cbbf
Ondřej Lysoněk <olysonek@redhat.com>
2018-08-13 14:39:43 +0200

    Make sure strings copied by strncpy are null-terminated
    Signed-off-by: Ondřej Lysoněk <olysonek@redhat.com>

--- src/minicom.c
+++ src/minicom.c
@@ -1208,6 +1208,7 @@ int main(int argc, char **argv)
           break;
         case 't': /* Terminal type */
           strncpy(termtype, optarg, sizeof(termtype));
+          termtype[sizeof(termtype) - 1] = '\0';
 #ifdef __GLIBC__
           /* Bug in older libc's (< 4.5.26 I think) */
           if ((s = getenv("TERMCAP")) != NULL && *s != '/')
@@ -1322,7 +1323,9 @@ int main(int argc, char **argv)
     strncpy(homedir, pwd->pw_dir, sizeof(homedir));
   else
     strncpy(homedir, s, sizeof(homedir));
+  homedir[sizeof(homedir) - 1] = '\0';
   strncpy(username, pwd->pw_name, sizeof(username));
+  username[sizeof(username) - 1] = '\0';
 
   /* Get personal parameter file */
   snprintf(pparfile, sizeof(pparfile), "%s/.minirc.%s", homedir, use_port);
